33875da857b4928eab2810cdc03a7b7adac2a040
Kasyap-R <kasyap.rachuri2@gmail.com>
2024-07-06 00:30:39 -0400

    Added cube

--- src/main.c
+++ src/main.c
@@ -49,13 +49,31 @@ i32 main() {
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
   float vertices[] = {
-      // positions          // colors           // texture coords
-      0.5f,  0.5f,  0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, // top right
-      0.5f,  -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // bottom right
-      -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom left
-      -0.5f, 0.5f,  0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f  // top left
-  };
+      -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 0.5f,  -0.5f, -0.5f, 1.0f, 0.0f,
+      0.5f,  0.5f,  -0.5f, 1.0f, 1.0f, 0.5f,  0.5f,  -0.5f, 1.0f, 1.0f,
+      -0.5f, 0.5f,  -0.5f, 0.0f, 1.0f, -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
 
+      -0.5f, -0.5f, 0.5f,  0.0f, 0.0f, 0.5f,  -0.5f, 0.5f,  1.0f, 0.0f,
+      0.5f,  0.5f,  0.5f,  1.0f, 1.0f, 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+      -0.5f, 0.5f,  0.5f,  0.0f, 1.0f, -0.5f, -0.5f, 0.5f,  0.0f, 0.0f,
+
+      -0.5f, 0.5f,  0.5f,  1.0f, 0.0f, -0.5f, 0.5f,  -0.5f, 1.0f, 1.0f,
+      -0.5f, -0.5f, -0.5f, 0.0f, 1.0f, -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+      -0.5f, -0.5f, 0.5f,  0.0f, 0.0f, -0.5f, 0.5f,  0.5f,  1.0f, 0.0f,
+
+      0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 0.5f,  0.5f,  -0.5f, 1.0f, 1.0f,
+      0.5f,  -0.5f, -0.5f, 0.0f, 1.0f, 0.5f,  -0.5f, -0.5f, 0.0f, 1.0f,
+      0.5f,  -0.5f, 0.5f,  0.0f, 0.0f, 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+      -0.5f, -0.5f, -0.5f, 0.0f, 1.0f, 0.5f,  -0.5f, -0.5f, 1.0f, 1.0f,
+      0.5f,  -0.5f, 0.5f,  1.0f, 0.0f, 0.5f,  -0.5f, 0.5f,  1.0f, 0.0f,
+      -0.5f, -0.5f, 0.5f,  0.0f, 0.0f, -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+
+      -0.5f, 0.5f,  -0.5f, 0.0f, 1.0f, 0.5f,  0.5f,  -0.5f, 1.0f, 1.0f,
+      0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+      -0.5f, 0.5f,  0.5f,  0.0f, 0.0f, -0.5f, 0.5f,  -0.5f, 0.0f, 1.0f};
+
+  // Unused currently
   unsigned int indices[] = {
       // note that we start from 0!
       0, 1, 3, // first triangle
@@ -81,14 +99,11 @@ i32 main() {
                GL_STATIC_DRAW);
 
   // Set vertex attribute pointers for coords, color, and texture
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)0);
   glEnableVertexAttribArray(0);
-  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float),
                         (void *)(3 * sizeof(float)));
   glEnableVertexAttribArray(1);
-  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
-                        (void *)(6 * sizeof(float)));
-  glEnableVertexAttribArray(2);
 
   // Unbind VAO and EBO (good practice)
   glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -125,11 +140,40 @@ i32 main() {
   // Create minmaps for currently bound texture
   glGenerateMipmap(GL_TEXTURE_2D);
 
-  // Set up transformation matrix
-  vec4 transform_vec = {0.0f, 0.0f, 0.0f, 1.0f};
-  mat4 transform_mat;
-  glm_mat4_identity(transform_mat);
-  glm_translate(transform_mat, transform_vec);
+  // Setting up view matrix
+  // note that we're translating the scene in the reverse direction of where we
+  // want to move (i.e. if we want to move forward, move the scene backward)
+  mat4 view_mat;
+  glm_mat4_identity(view_mat);
+  vec3 view_vec = {0.0, 0.0f, -3.0f};
+  glm_translate(view_mat, view_vec);
+
+  // Setting up projection matrix
+  mat4 projection_mat;
+  glm_mat4_identity(projection_mat);
+  glm_perspective(glm_rad(95.0f), 800.0f / 600.0f, 0.1f, 100.0f,
+                  projection_mat);
+
+  // Enabel Depth Testing
+  glEnable(GL_DEPTH_TEST);
+
+  // Set up Camera
+  vec3 cameraPos = {0.0f, 0.0f, 3.0f};
+  vec3 cameraTarget = {0.0f, 0.0f, 0.0f};
+  vec3 cameraDirection;
+  glm_vec3_zero(cameraDirection);
+  glm_vec3_sub(cameraPos, cameraTarget, cameraDirection);
+  glm_normalize(cameraDirection);
+
+  vec3 up = {0.0f, 1.0f, 0.0f};
+  vec3 cameraRight;
+  glm_vec3_zero(cameraRight);
+  glm_cross(cameraDirection, cameraRight, cameraRight);
+  glm_normalize(cameraRight);
+
+  vec3 cameraUp;
+  glm_vec3_zero(cameraUp);
+  glm_cross(cameraDirection, cameraRight, cameraUp);
 
   // Uncomment to draw in wireframe mode
   // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
@@ -140,9 +184,22 @@ i32 main() {
 
     // Clear the screen
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    // Use the shader program and draw the triangle
+    // Setting up model matrix
+    mat4 model_mat;
+    glm_mat4_identity(model_mat);
+    vec3 rotation_axis = {0.5f, 1.0f, 0.0f}; // x-axis
+    glm_rotate(model_mat, (float)glfwGetTime() * glm_rad(50.0f), rotation_axis);
+
+    // Setting up transformation matrix
+    mat4 transform_mat;
+    glm_mat4_identity(transform_mat);
+    glm_mat4_mul(model_mat, transform_mat, transform_mat);
+    glm_mat4_mul(view_mat, transform_mat, transform_mat);
+    glm_mat4_mul(projection_mat, transform_mat, transform_mat);
+
+    // Use the shader program
     glUseProgram(shader);
 
     // Set transformation matrix in vertex shader
@@ -151,7 +208,8 @@ i32 main() {
 
     glBindVertexArray(VAO);
     glBindTexture(GL_TEXTURE_2D, texture);
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glDrawArrays(GL_TRIANGLES, 0, 36);
+    // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     glBindVertexArray(0);
 
     // Swap buffers and poll events
